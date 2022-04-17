cb9f539cee3e6a9cfebbe15867d87537a27a954c
Zorca Orcinus <vs@zorca.org>
2022-04-17 11:02:28 +0400

    add: fillable properties for model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'level1',
+        'level2',
+        'level3',
+        'price',
+        'price_sp',
+        'amount',
+        'properties',
+        'joint_purchase',
+        'unit',
+        'picture',
+        'show_on_main_page',
+        'description'
+    ];
 }
